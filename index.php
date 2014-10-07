656cf137a5b4c815c1bbe138b21a75325bff1ad1
zeitzbach <zeitzbach@Gmail.com>
2014-10-07 13:49:10 +0700

    Index tweak
    Added the Shopping and Inventory buttons back in.

--- index.php
+++ index.php
@@ -15,9 +15,10 @@ background-image: url(http://i.imgur.com/KjfSAxM.png)
    <div style="background-color: pink; margin: 10px">
         <Center><font size = "25">KU-RELATIONSHIP</font></Center><br>
 			<div align="right">
-
-			<a href="?page=login"><img src="http://i.imgur.com/ivQ59CY.png" /></a>
-			<a href="?page=signup"><img src="http://i.imgur.com/c9GPkX7.png" /></a> </div>
+			<a href="?page=shopping"><img src="http://i.imgur.com/KePc1Ps.png"/></a>
+			<a href="?page=inventory"><img src="http://i.imgur.com/FRzI8aj.png"/></a>
+			<a href="?page=login"><img src="http://i.imgur.com/ivQ59CY.png"/></a>
+			<a href="?page=signup"><img src="http://i.imgur.com/c9GPkX7.png"/></a> </div>
 
 			</div>
     </div>
