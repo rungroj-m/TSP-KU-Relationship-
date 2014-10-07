3bdfaf52134bde128d454c103932d7b5844fa2da
zeitzbach <zeitzbach@Gmail.com>
2014-10-07 12:03:29 +0700

    Index : Added spot to put search bar
    Added another div to put search bar along with pic of the search button.

--- index.php
+++ index.php
@@ -12,12 +12,25 @@ background-image: url(http://i.imgur.com/KjfSAxM.png)
 <body>
 
     <!-- static for each page -->
-   <div style="background-color: pink; margin: 20px">
+   <div style="background-color: pink; margin: 10px">
         <Center><font size = "25">KU-RELATIONSHIP</font></Center><br>
 			<div align="right">
 
-			<a href="?page=login"><img src="http://i.imgur.com/ivQ59CY.png" />  </a>
-			<a href="?page=signup"><img src="http://i.imgur.com/c9GPkX7.png" />  </a> </div>
+			<a href="?page=login"><img src="http://i.imgur.com/ivQ59CY.png" /></a>
+			<a href="?page=signup"><img src="http://i.imgur.com/c9GPkX7.png" /></a> </div>
+
+			</div>
+    </div>
+    
+	 <div style="background-color: pink; margin: 10px">
+        <Center><font size = "5">SEARCH</font></Center><br>
+			<div align="center">
+			
+			<a href="?page=login"><img src="http://i.imgur.com/E1EINEg.png" /></a>
+			<a> <- Category here [searchbar here] -> button pic </a>
+			<a href="?page=login"><img src="http://i.imgur.com/E1EINEg.png" /></a></div>
+			
+			
 
 			</div>
     </div>
