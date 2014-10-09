5ea819114eaaa05ad64ef21f7fd367cfbcb0b67e
benzsuankularb <benzsk130@gmail.com>
2014-10-09 11:46:36 +0700

    update db

--- inc/ecomerce.sql
+++ inc/ecomerce.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 01, 2014 at 01:25 PM
+-- Generation Time: Oct 09, 2014 at 06:45 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,55 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Brands`
---
-
-CREATE TABLE IF NOT EXISTS `Brands` (
-`BrandId` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=38 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Categories`
---
-
-CREATE TABLE IF NOT EXISTS `Categories` (
-`CategoryId` int(11) NOT NULL,
-  `Name` varchar(50) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ProductDescriptions`
---
-
-CREATE TABLE IF NOT EXISTS `ProductDescriptions` (
-`ProductDescriptionId` int(11) NOT NULL,
-  `CategoryId` int(11) NOT NULL,
-  `BrandId` int(11) NOT NULL,
-  `ProductName` text NOT NULL,
-  `ModelCode` text NOT NULL,
-  `Description` text NOT NULL,
-  `CreateDate` datetime NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ProductDescriptionTags`
---
-
-CREATE TABLE IF NOT EXISTS `ProductDescriptionTags` (
-  `ProductDescriptionId` int(11) NOT NULL,
-  `TagId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `Products`
 --
 
@@ -81,40 +32,18 @@ CREATE TABLE IF NOT EXISTS `Products` (
   `Price` float NOT NULL,
   `CreateDate` datetime NOT NULL,
   `Status` tinyint(1) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
--- Table structure for table `Tags`
+-- Dumping data for table `Products`
 --
 
-CREATE TABLE IF NOT EXISTS `Tags` (
-`TagId` int(11) NOT NULL,
-  `Key` varchar(100) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=109 ;
+INSERT INTO `Products` (`ProductId`, `ProductDescriptionId`, `Price`, `CreateDate`, `Status`) VALUES
+(1, '1', 10, '2014-10-09 11:40:45', 1);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `Brands`
---
-ALTER TABLE `Brands`
- ADD PRIMARY KEY (`BrandId`), ADD UNIQUE KEY `name` (`Name`);
-
---
--- Indexes for table `Categories`
---
-ALTER TABLE `Categories`
- ADD PRIMARY KEY (`CategoryId`), ADD UNIQUE KEY `name` (`Name`);
-
---
--- Indexes for table `ProductDescriptions`
---
-ALTER TABLE `ProductDescriptions`
- ADD PRIMARY KEY (`ProductDescriptionId`);
 
 --
 -- Indexes for table `Products`
@@ -123,40 +52,14 @@ ALTER TABLE `Products`
  ADD PRIMARY KEY (`ProductId`);
 
 --
--- Indexes for table `Tags`
---
-ALTER TABLE `Tags`
- ADD PRIMARY KEY (`TagId`), ADD UNIQUE KEY `Key` (`Key`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `Brands`
---
-ALTER TABLE `Brands`
-MODIFY `BrandId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=38;
---
--- AUTO_INCREMENT for table `Categories`
---
-ALTER TABLE `Categories`
-MODIFY `CategoryId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=30;
---
--- AUTO_INCREMENT for table `ProductDescriptions`
---
-ALTER TABLE `ProductDescriptions`
-MODIFY `ProductDescriptionId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
---
 -- AUTO_INCREMENT for table `Products`
 --
 ALTER TABLE `Products`
-MODIFY `ProductId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
---
--- AUTO_INCREMENT for table `Tags`
---
-ALTER TABLE `Tags`
-MODIFY `TagId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=109;
+MODIFY `ProductId` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
